9b868470249f806db3abfd9fd38fe2c9849e5970
Kiryllog <kirik.by.ru@gmail.com>
2020-11-26 03:36:48 +0300

    home commit three

---
+++ homework3/src/main/java/by/antonau/homework3/FirstScreenActivity.kt
@@ -0,0 +1,11 @@
+package by.antonau.homework3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FirstScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.android_cource)
+    }
+}
